e5209fe3bc1a9a16dc471f06b09b7eac73c0245f
BHAVYA96509 <22103085@mail.jiit.ac.in>
2024-08-06 15:25:56 +0530

    Add files via upload

---
+++ Untitled1.c
@@ -0,0 +1,36 @@
+// C Program to Reverse a String using Two-Pointer Technique
+#include <stdio.h>
+#include <string.h>
+
+void reverseString(char* str) {
+
+    // Initialize start and end pointers
+    int start = 0;
+    int end = strlen(str) - 1;
+    char temp;
+
+    // Swap characters while start is less than end
+    while (start < end) {
+
+        // Swap characters
+        temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+
+        // Move pointers
+        start++;
+        end--;
+    }
+}
+
+int main()
+{
+    char str[100] = "Hello World";
+    printf("Original String: %s\n", str);
+
+    reverseString(str);
+    printf("Reversed string: %s\n", str);
+
+    return 0;
+}
+
